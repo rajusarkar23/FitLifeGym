675ccff1810519615fd14d6a8c9762de96007ecc
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-05-25 14:06:05 +0530

    fix build err

--- frontend/components/member-mobile-nav.tsx
+++ frontend/components/member-mobile-nav.tsx
@@ -4,8 +4,6 @@ import * as React from "react";
 import Link from "next/link";
 import { Menu } from "lucide-react";
 
-import Cookies from "js-cookie";
-
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
